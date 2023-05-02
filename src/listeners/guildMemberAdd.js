db4d7e0c7620851994eceb9199c5d51982292256
Kheeto <kheetoxd@gmail.com>
2023-05-02 20:10:40 +0200

    Auto Roles on member join

---
+++ src/listeners/guildMemberAdd.js
@@ -0,0 +1,19 @@
+const Logger = require("../Logger");
+
+const autoRolesEnabled = false;
+const autoRolesList = [""];
+
+module.exports = {
+    name: 'guildMemberAdd',
+    async execute(member, client)
+    {
+        // Auto roles
+        if (autoRolesEnabled) {
+            Logger.Info(`[AUTOROLES] Adding join auto roles to ${member.user.tag}`);
+            autoRolesList.forEach(async (role) => {
+                Logger.Info(`[AUTOROLES] Adding role ${member.guild.roles.cache.get(role).name} to ${member.user.tag}`);
+                await member.roles.add(role);
+            });
+        }
+    }
+}
