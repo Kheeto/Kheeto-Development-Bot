30e51ddd13f3569202fa52a8711a3b3319b5a6d7
Kheeto <kheetoxd@gmail.com>
2023-05-24 16:41:52 +0200

    Update rolepanel.js

--- src/commands/rolepanel.js
+++ src/commands/rolepanel.js
@@ -20,45 +20,37 @@ module.exports = {
             return;
         }
 
-        const row = new ActionRowBuilder();
-        const selectionMenu = new SelectMenuBuilder()
-        .setCustomId("roleSelectionMenu")
-        .setPlaceholder("Select the roles you want to have!")
-        .addOptions(
-            {
-                label: "First role",
-                description: "Select the first role",
-                value: "first_role",
-            },
-            {
-                label: "Second role",
-                description: "Select the second role",
-                value: "second_role",
-            },
-            {
-                name: "Third role",
-                description: "Select the third role",
-                value: "third_role",
-            },
-            {
-                label: "Fourth role",
-                description: "Select the fourth role",
-                value: "fourth_role",
-            },
-            {
-                name: "Fifth role",
-                description: "Select the fifth role",
-                value: "fifth_role",
-            }
-        );
+        try {
+            const row = new ActionRowBuilder()
+            .addComponents(
+                new SelectMenuBuilder()
+                .setCustomId("roleSelectionMenu")
+                .setMinValues(0)
+                .setMaxValues(2)
+                .setPlaceholder("Nothing selected")
+                .addOptions(
+                    {
+                        label: 'First role',
+                        description: "Select the first role",
+                        value: "first_role",
+                    },
+                    {
+                        label: 'Second role',
+                        description: "Select the second role",
+                        value: "second_role",
+                    }
+                ),
+            );
 
-        row.addComponents(selectionMenu);
+            const embed = new EmbedBuilder()
+            .setTitle("Role selection")
+            .setDescription("Pick the roles you want to have!");
 
-        const embed = new EmbedBuilder()
-        .setTitle("Role selection")
-        .setDescription("You can select the roles you want to have and they will be added or removed to you!");
-
-        await interaction.channel.send({ embeds: [embed], components: [row] });
-        await interaction.reply({ content: "Successfully created a new role selection panel!", ephemeral: true });
+            await interaction.channel.send({ embeds: [embed], components: [row] });
+            await interaction.reply({ content: "Successfully created a new role selection panel!", ephemeral: true });
+        }
+        catch (err) {
+            console.log(err);
+        }
     }
 }
