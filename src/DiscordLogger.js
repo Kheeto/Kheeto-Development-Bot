9b7fbcb6b731b7751e978362c46b50d8cfb3fbb7
Kheeto <kheetoxd@gmail.com>
2023-05-24 11:28:35 +0200

    Update DiscordLogger.js

--- src/DiscordLogger.js
+++ src/DiscordLogger.js
@@ -1,7 +1,7 @@
 const { TextChannel } = require("discord.js");
 const { actionLogEnabled, actionLogChannel, moderationLogEnabled, moderationLogChannel } = require("../config/config.json");
 const Action = actionLogChannel; // Default action log channel ID
-const Moderation = moderationLogChannel; // Default action log channel ID
+const Moderation = moderationLogChannel; // Default moderation log channel ID
 
 async function Log(logChannel, embed) {
     if (logChannel == Action && !actionLogEnabled) return;
