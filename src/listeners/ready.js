11bdecc639d67e7b2d4b03402a81ffbabea56b95
Kheeto <kheetoxd@gmail.com>
2023-05-26 17:32:15 +0200

    MongoDB connection

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,11 +1,14 @@
 const Logger = require("../Logger");
 const { ActivityType } = require('discord.js');
 const { statusText, statusType } = require('../../config/config.json');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const mongodb = process.env.MONGODB;
 
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client)
+    async execute(client)
     {
         Logger.Info("[SETUP] Setting rich presence and getting the bot ready");
         client.user.setPresence({
@@ -18,5 +21,18 @@ module.exports = {
             status: statusType,
         });
         Logger.Success(`[INFO] Bot is now online! Logged in as ${client.user.username}`);
+
+        // Connect to database
+        if (!mongodb) {
+            return Logger.Error("[ERROR] MONGODB environment variable is missing in the .env file - Couldn't connect to the database!");
+        }
+        await mongoose.connect(mongodb || '', {
+            keepAlive: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        if (mongoose.connect) {
+            Logger.Success("[INFO] Connected to database.");
+        }
     },
 };
